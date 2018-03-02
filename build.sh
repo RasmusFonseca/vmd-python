d1892066b4a3756860d7503f9cd795052d52347c
Robin Betz <robin@robinbetz.com>
2018-03-02 11:56:09 -0800

    Forgot build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Removing homebrew"
+/usr/bin/sudo /usr/bin/find /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/var -name .DS_Store -delete
+
+/usr/bin/sudo /usr/bin/find /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/var -depth -type d -empty -exec rmdir {} ;
+
+echo "BEGIN DEBUG"
+echo "XCODES AVAILABLE:"
+xcode-select --print-path
+
+#echo "INSTALLING?"
+#xcode-select --install
+
+echo "CC IS: $(which cc)"
+echo "CLANG IS: $(which clang)"
+echo "TRYNA RUN CC:"
+cc
+
+python -m pip install --no-deps --ignore-installed .
