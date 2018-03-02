35f93ea76554b42d1afd844bd2a380fd2b10ff31
Robin Betz <robin@robinbetz.com>
2018-03-02 14:32:05 -0800

    Try deleting homebrew properly

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Removing homebrew"
-/usr/bin/sudo /usr/bin/find /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/var -name .DS_Store -delete
-
-/usr/bin/sudo /usr/bin/find /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/var -depth -type d -empty -exec rmdir {} ;
-
 echo "BEGIN DEBUG"
 echo "XCODES AVAILABLE:"
 xcode-select --print-path
@@ -19,4 +14,6 @@ echo "CLANG IS: $(which clang)"
 echo "TRYNA RUN CC:"
 cc
 
-python -m pip install --no-deps --ignore-installed .
+python setup.py build
+python setup.py install
+#python -m pip install --no-deps --ignore-installed .
