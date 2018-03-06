25f0bdf0b11747b11a7b79157fa6dc149cf3d2d2
Robin Betz <robin@robinbetz.com>
2018-03-06 11:59:47 -0800

    Just paste run_conda_forge_build_setup contents into setup_build

--- setup_build.sh
+++ setup_build.sh
@@ -25,7 +25,23 @@ echo "Configuring conda."
 conda config --add channels conda-forge
 conda config --set show_channel_urls true
 conda install --yes --quiet conda-build-all
-conda install --yes --quiet conda-forge-build-setup
-source ~/miniconda/bin/run_conda_forge_build_setup
 
+echo "Running build setup thing"
+# Contents of run_conda_forge_build_setup follow
 
+# 2 cores available on CircleCI workers: https://discuss.circleci.com/t/what-runs-on-the-node-container-by-default/1443
+# CPU_COUNT is passed through conda build: https://github.com/conda/conda-build/pull/1149
+export CPU_COUNT=2
+
+export PYTHONUNBUFFERED=1
+
+conda config --set show_channel_urls true
+conda config --set auto_update_conda false
+conda config --set add_pip_as_python_dependency false
+
+conda update -n root --yes --quiet conda conda-env conda-build
+conda install -n root --yes --quiet jinja2 anaconda-client
+conda install -n root --yes --quiet conda-build=2
+
+conda info
+conda config --get
